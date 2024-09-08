72aeade4d0a482517c77bddcdd781474d9bbf3a9
Zac <579103+7a6163@users.noreply.github.com>
2024-09-08 19:10:51 +0800

    Update Docker tags and geminabox version to 2.1.0

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 image=7a6163/geminabox
-geminabox_version=1.4.1
+geminabox_version=2.1.0
 
 docker build -t $image:$geminabox_version . --no-cache
 docker tag $image:$geminabox_version $image:latest
